05a274dd2b5d572b7be7e655be945e94cfaa8db4
kabosnn <anagynandor@gmail.com>
2024-06-20 14:48:21 +0200

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [ './pages/**/*.{html,tsx}',
+    './components/**/*.{html,tsx}'],
   theme: {
     extend: {},
   },
